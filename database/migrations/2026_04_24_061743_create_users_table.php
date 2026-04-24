919d408953b8b105aa6388a6315ca4ae9dc841ca
danyakaari <snazovd@gmail.com>
2026-04-24 00:31:50 -0700

    Лаба 7: миграции для users и statya

--- database/migrations/2026_04_24_061743_create_users_table.php
+++ database/migrations/2026_04_24_061743_create_users_table.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->date('bithrday');
+        });
     }
 
     /**
@@ -19,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('users');
     }
 };
